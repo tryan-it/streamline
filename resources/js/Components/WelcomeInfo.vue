84331564e55907b026b22ff0b7b59aa8d4cea719
Thomas Ryan <tommyjohnryan@gmail.com>
2022-02-08 14:47:09 -0800

    Initial layout, added contact info with map

---
+++ resources/js/Components/WelcomeInfo.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+        <div class="relative sm:max-w-sm w-full">
+            <div class="card shadow-lg bg-red absolute w-full h-full text-center rotate-12"></div>
+            <div class="card shadow-lg bg-cyan absolute w-full h-full text-center -rotate-12"></div>
+            <div class="relative w-full rounded-3xl px-6 py-4 bg-gray-100 shadow-md">Content</div>
+        </div>
+    </div>
+</template>
